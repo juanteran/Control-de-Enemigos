022b69180a2f552f166f909953f0a5836bc5e884
Juan Carlos Terán <v28275914@gmail.com>
2017-11-03 15:08:26 -0430

    * clsEnemigo.cs:
    * EjercicioSC.csproj: Clase de Enemigo Creada.

---
+++ EjercicioSC/clsEnemigo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace EjercicioSC
+{
+	public class clsEnemigo
+	{
+		public int Id { get; set; }
+		public string Nombre { get; set; }
+		public float Vida { get; set; }
+
+		//Falta crear posición
+
+		public clsEnemigo()
+		{
+		}
+	}
+}
